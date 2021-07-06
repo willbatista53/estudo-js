15adf91a6400aee64b5899cdb2dad93e8260e1cb
William Batista <williambatista53@hotmail.com>
2021-07-06 20:16:58 -0300

    aula15 - funcao Math

---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.54578;
+//console.log(!!(100 / 0));
+
+//let num2 = Math.floor(num1);
+//let num2 = math.ceil(num1);
+//let num2 = Math.round(num1);
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+//const aleatório = Math.round(Math.random() * (10 - 5) + 5);
